a89bc92771c56ed03577a669cbf78cc3ff4f24e3
Joanne Nueva <97574396+JRNueva@users.noreply.github.com>
2025-10-07 14:09:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from controller.address_book import Contacts
       
 # Contacts("json").display_contacts()
 
-Contacts("json").search_contacts("John")
+Contacts("json").search_contacts("Jane")
--- controller/address_book.py
+++ controller/address_book.py
@@ -6,13 +6,13 @@ class Contacts:
         self.contact_bll = ContactBll(source)
     
     def display_contacts(self):
-        for record in self.contact_bll.retrieve_contacts().get("contacts"):
+        for record in self.contact_bll.retrieve_contacts().get("contacts",[]):
             print(f"Name: {record.get('name')}")
             print(f"Contact No: {record.get('contact_no')}")
             print()
             
     def search_contacts(self, keyword):
-        for record in self.contact_bll.search_contacts(keyword).get("contacts"):
+        for record in self.contact_bll.search_contacts(keyword).get("contacts",[]):
             print(f"Name: {record.get('name')}")
             print(f"Contact No: {record.get('contact_no')}")
             print()
